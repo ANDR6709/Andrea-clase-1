b36264e214fe568520ee419af5d81f7035577f43
ANDR6709 <andreatovar549@gmail.com>
2023-11-11 18:45:27 -0500

    mi primera practica en git bash

--- holamundo.py
+++ holamundo.py
@@ -1,4 +1,50 @@
+# numero
+
 number=5
 print('el valor de la variable es:',number)
 texto='texto'
 print('el valor de la variable es:',texto)
+
+
+
+
+
+# Calcular el area de un cuadrado
+
+
+
+
+
+lado = (input("ingrese uno de los lados del cuadrado:" ))
+lado= float (lado)
+area= lado*lado
+print(f"el area de un cuadrado es:{area}cm") 
+
+
+
+
+# Pedir al usuario que ingrese la base del triángulo
+base = float (input("Ingrese la longitud de la base del triángulo: "))
+
+# Pedir al usuario que ingrese la altura del triángulo
+altura = float (input("Ingrese la altura del triángulo: "))
+
+# Calcular el área del triángulo
+area = (base * altura) / 2
+
+# Mostrar el resultado
+print("El área del triángulo es:",area)
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
